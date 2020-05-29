ea58d8ec65f3b5d3ee5d6ce180472d22500794c2
Andrew Mahoney <andrew.mahoney@kaboodle.co.uk>
2020-05-29 13:07:37 +0000

    GetPrimaryKey from schema

--- src/Phinx/Db/Adapter/MysqlAdapter.php
+++ src/Phinx/Db/Adapter/MysqlAdapter.php
@@ -697,6 +697,7 @@ class MysqlAdapter extends PdoAdapter
      */
     public function getPrimaryKey($tableName)
     {
+        $options = $this->getOptions();
         $rows = $this->fetchAll(sprintf(
             "SELECT
                 k.constraint_name,
@@ -705,7 +706,9 @@ class MysqlAdapter extends PdoAdapter
             JOIN information_schema.key_column_usage k
                 USING(constraint_name,table_name)
             WHERE t.constraint_type='PRIMARY KEY'
+                AND t.table_schema='%s'
                 AND t.table_name='%s'",
+            $options['name'],
             $tableName
         ));
 
